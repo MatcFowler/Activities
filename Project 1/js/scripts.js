6c7162291302f8cc795ef0c475bfb5666d4517e1
MatcFowler <70304081+MatcFowler@users.noreply.github.com>
2020-10-27 12:23:36 -0500

    Add files via upload

---
+++ Project 1/js/scripts.js
@@ -0,0 +1,7 @@
+var User = "Joshua Fowler";
+var date = "10/20/2020";
+var info = "Website Author: " + User + "    Last update date: " + date;
+
+    infoEL = document.getElementById('in');
+
+infoEL.textContent=info;
